2e602293a79024e7b44199abdaa4275852fb75a2
Katzmann835 <55995833+Katzmann835@users.noreply.github.com>
2026-02-18 15:27:20 -0500

    Made DOM_Work.github.io more efficient
    Got rid of lines that were not contributing to the functionality of the code, placed classes with styles into one element, made the functions more efficient.

--- DOM_work/DOM_work.js
+++ DOM_work/DOM_work.js
@@ -1,27 +1,49 @@
-const One = document.getElementsByClassName('Red_square')
-const Total_One = One.length
-for (let i = 0; i < One.length; i++){
-    One[i].style.backgroundcolor = "red"
+const squares = [
+    {
+        className: 'Red_square',
+        styles: {
+            backgroundcolor: 'red',
+        }
+    },
+    {
+        className: 'Yellow_square',
+        styles: {
+            backgroundcolor: 'yellow',
+            color: 'black',
+        }
+    },
+    {
+        className: 'Indigo_square',
+        styles: {
+            backgroundcolor: 'indigo',
+            color: 'white',
+        }
+    },
+    {
+        className: 'Magenta_square',
+        styles: {
+            backgroundcolor: 'magenta',
+         }
+    }
+ ];
+
+const first_amount(squares) = document.getElementsbyClassName('Red_square')
+for (let i = 0; i < squares.length; i++){
+    first_amount[i].style.backgroundcolor = "red"
 }
 
-const Two = document.getElementsByClassName('Yellow_square')
-const Total_Two = Two.length
-for (let i = 0; i < Two.length; i++){
-    Two[i].style.backgroundcolor = 'yellow'
-    Two[i].style.color = 'black'
+const second_amount(squares) = document.getElementsbyClassName('Yellow_square')
+for (let i = 0; i < squares.length; i++){
+    second_amount[i].style.backgroundcolor = "yellow"
+    second_amount[i].style.color = "black"
+}
+const third_amount(squares) = document.getElementsbyClassName('Indigo_square')
+for (let i = 0; i < squares.length; i++){
+    third_amount[i].style.backgroundcolor = "indigo"
+    second_amount[i].style.color = "white"
 }
 
-const Three = document.getElementsByClassName('Indigo_square')
-const Total_Three = Three.length
-for (let i = 0; i < Three.length; i++){
-    Three[i].style.backgroundcolor = 'indigo'
-    Three[1].style.color = 'white'
+const fourth_amount(squares) = document.getElementsbyClassName('Magenta_square')
+for (let i = 0; i < squares.length; i++){
+    fourth_amount[i].style.backgroundcolor = "magenta"
 }
-
-const Four = document.getElementsByClassName('Magenta_square')
-const Total_Four = Four.length
-for (let i = 0; i < Four.length; i++){
-    Four[i].style.backgroundcolor = 'magenta'
-}
-
-
