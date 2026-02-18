093c2827b373dd267c8ae4641e6f31d31189db11
Katzmann835 <55995833+Katzmann835@users.noreply.github.com>
2026-02-18 15:48:10 -0500

    Modified DOM_work.js

--- DOM_work/DOM_work.js
+++ DOM_work/DOM_work.js
@@ -27,23 +27,15 @@ const squares = [
     }
  ];
 
-const first_amount(squares) = document.getElementsbyClassName('Red_square')
-for (let i = 0; i < squares.length; i++){
-    first_amount[i].style.backgroundcolor = "red"
-}
-
-const second_amount(squares) = document.getElementsbyClassName('Yellow_square')
-for (let i = 0; i < squares.length; i++){
-    second_amount[i].style.backgroundcolor = "yellow"
-    second_amount[i].style.color = "black"
-}
-const third_amount(squares) = document.getElementsbyClassName('Indigo_square')
-for (let i = 0; i < squares.length; i++){
-    third_amount[i].style.backgroundcolor = "indigo"
-    second_amount[i].style.color = "white"
-}
-
-const fourth_amount(squares) = document.getElementsbyClassName('Magenta_square')
-for (let i = 0; i < squares.length; i++){
-    fourth_amount[i].style.backgroundcolor = "magenta"
-}
+const styleElements = (elements) => {
+    elements.forEach((item) => {
+        const dom_elements = document.getElementsByClassName(item.className);
+        for (let i = 0; i < dom_elements.length; i++){
+            const el = dom_elements[i];
+            for (let j in item.styles){
+                el.style[j] = item.styles[j];
+            }
+        }
+    })
+};
+styleElements(squares);
